a1cacd2399c41e7ecb2663ec023e30e654fa8d28
Changwan Sun <tjsckddhks@gmail.com>
2021-05-17 21:04:30 +0900

    upddate that I can image B_scan_image with gmt!

--- B_SCAN.sh
+++ B_SCAN.sh
@@ -6,7 +6,12 @@ file=B_SCAN_IMAGE_GPR.txt
 #region coloumns/records | columns are fields and records are rows
 
 #       type them in a ascending orer
+#400 x 200
+# 400*0.05
+#0, 20 
+#region=-R0/399/-199/0
 region=-R0/399/-199/0
+
 
 
 #gmt makecpt -Crainbow -T-3590336.0/3103296.0/100.0 > colors.cpt
@@ -18,7 +23,8 @@ gmt begin b_scanf png
  
      gmt xyz2grd $file $region -Gb_scanf.grd -I1/1 -ZTLA
      gmt grdimage b_scanf.grd $region -BWS  -Cvik
-     gmt basemap $region -Bx50+l"Distance (m)" -By50 -BWseN
+     gmt basemap $region -Bx50+l"Scan" -By50+l"Sample" -BWseN
+
 gmt end show
 
 
